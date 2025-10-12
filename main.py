e4d3ed30d5cfe818607ac3ce91dc26172c554a9e
Alena Kucherenko <alenka110198@gmail.com>
2025-10-12 13:53:05 +0600

    update

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import flet as ft
+from datetime import datetime
 
 
 def main(page: ft.Page): # создаём главную страницу приложения
@@ -7,29 +8,49 @@ def main(page: ft.Page): # создаём главную страницу при
 
     greeting_text = ft.Text('Hello world!') # текст на главной странице
     name_input = ft.TextField(label='Введите имя') # создаём текстовое поле
+    greeting_history = []
+    history_text = ft.Text('История приветствий: ')
+
+
+    # def updade_history():
+    #     history_controls = [ft.Text('История приветствий: ')]
 
     def one_button_click(_): # функция для ввода и замены заголовка
         name = name_input.value.strip()
-        print(name)
 
         if name:
-            print(greeting_text)
             greeting_text.value = f'Hello {name}' # меняем заголовок
-            print(greeting_text)
             name_input.value = '' # очищаем поле ввода
+            timestamp = datetime.now().strftime('%M:%S')
+
+            greeting_history.append(f'{timestamp} - {name}') # фиксируем время ввода
+            history_text.value = 'История приветствий:\n' + '\n'.join(greeting_history)
+
         else:
             print('Ничего не введено')
             greeting_text.value = 'Пожалуйста, введите имя'
         page.update()
 
+    def clear_history(_): # функция для очистки списка
+        greeting_history.clear()
+        history_text.value = 'История приветствий:'
+        page.update()
+
     def change_theme(_): # функция для смены темы
         page.theme_mode = ft.ThemeMode.LIGHT if page.theme_mode ==  ft.ThemeMode.DARK else ft.ThemeMode.DARK
         page.update()
+    
+    name_input.on_submit = one_button_click # отправка по enter
 
     name_button = ft.ElevatedButton('Отправить', on_click=one_button_click) # создаём кнопку
-    theme_buttom = ft.IconButton(icon=ft.Icons.SUNNY, on_click=change_theme)
+    theme_button = ft.IconButton(icon=ft.Icons.SUNNY, on_click=change_theme)
+    clear_button = ft.IconButton(icon=ft.Icons.DELETE, on_click=clear_history)
 
-    page.add(greeting_text, name_input, name_button, theme_buttom) # добавляем элементы на страницу
+    # page.add(greeting_text, name_input, name_button, theme_buttom, clear_buttom, history_text) # добавляем элементы на страницу
+    page.add(greeting_text, name_input,
+              ft.Row([name_button, theme_button, clear_button], alignment=ft.MainAxisAlignment.CENTER),
+              history_text
+              )
 
 
-ft.app(target=main, view=ft.WEB_BROWSER) # запуск приложения
+ft.app(target=main) # запуск приложения
